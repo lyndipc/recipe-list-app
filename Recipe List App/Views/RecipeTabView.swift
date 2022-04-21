dc6298166fca9ebc0b10dc40d12a20e45b1a471e
Lyndi Castrejon <lyndic@protonmail.com>
2022-04-21 16:38:09 -0500

    Add RecipeTabView.swift and edit main tab view

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,35 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by Lyndi Castrejon on 4/21/22.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        TabView {
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                    }
+                    Text("List")
+                }
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
